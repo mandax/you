a1fcff666a342d6d7234f82ff596eade1e6f1f9b
Anderson F. Pinto <anderson@afp.sh>
2026-07-24 19:59:04 -0300

    feat(ui): triadic azure/magenta/lime palette, vendored fonts, code highlight colors
    Space Grotesk display, Inter body, JetBrains Mono code as vendored woff2.
    Makeup + makeup_elixir for server-side syntax highlighting.

--- mix.exs
+++ mix.exs
@@ -84,6 +84,8 @@ defmodule You.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 1.0"},
       {:jason, "~> 1.2"},
+      {:makeup, "~> 1.1"},
+      {:makeup_elixir, "~> 1.0"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
       {:wax_, "~> 0.7.0"}
